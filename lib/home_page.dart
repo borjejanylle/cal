1e0dcfa11fb8aa9bd4e5deaa96091f651bcc13ec
PransLoren <jaaq.borje.up@phinmaed.com>
2024-07-19 23:10:40 +0800

    sekund

---
+++ lib/home_page.dart
@@ -0,0 +1,112 @@
+  import 'package:flutter/material.dart';
+
+  class HomePage extends StatefulWidget {
+    const HomePage({super.key});
+
+    @override
+    State<HomePage> createState() => _HomePageState();
+  }
+
+  class _HomePageState extends State<HomePage> {
+    final TextEditingController firstController = TextEditingController();
+    final TextEditingController secondController = TextEditingController();
+    int result = 0;
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Calculator App'),
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 10,
+              ),
+              TextField(
+                controller: firstController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(), hintText: 'First Number'),
+                keyboardType: TextInputType.number,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextField(
+                controller: secondController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(), hintText: 'Second Number'),
+                keyboardType: TextInputType.number,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      int a = int.parse(firstController.text);
+                      int b = int.parse(secondController.text);
+
+                      setState(() {
+                        result = a + b;
+                      });
+
+                    },
+                    child: const Text('Add'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      int a = int.parse(firstController.text);
+                      int b = int.parse(secondController.text);
+
+                      setState(() {
+                        result = a - b;
+                      });
+                    },
+                    child: const Text('Subtract'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      int a = int.parse(firstController.text);
+                      int b = int.parse(secondController.text);
+
+                      setState(() {
+                        result = a * b;
+                      });
+                    },
+                    child: const Text('Multiply'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      int a = int.parse(firstController.text);
+                      int b = int.parse(secondController.text);
+
+                      setState(() {
+                        if (b != 0) {
+                          result = a ~/ b; // Use integer division
+                        } else {
+                          // Handle division by zero error if necessary
+                          // For simplicity, this example assumes b is not zero
+                        }
+                      });
+                    },
+                    child: const Text('Divide'),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text('Result: $result',
+                style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        ),
+      );
+    }
+  }
